30401b946289c80c1ca0fd0315a66ff544884619
kiyose <kiyose408@qq.com>
2025-11-09 14:55:06 +0800

    package-test

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setOrganizationName("KIYOSE");
+    QApplication::setApplicationName("FastChat");
+    QApplication::setApplicationVersion("0.1.0-test");
     QApplication app(argc, argv);
     Logger::init(); //初始化日志
     Logger::info("Application started");
