7cb5a981bfdf44b7506f9c3e513c5edcfc3268ed
kiyose <kiyose408@qq.com>
2025-11-11 15:42:32 +0800

    modal login

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "ui/chatmainwindow.h"
 #include "utils/logger.h"
+#include "ui/logindialog.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -10,9 +11,25 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     Logger::init(); //初始化日志
     Logger::info("Application started");
-    ChatMainWindow w;
+    //创建并且显示登录对话框
+    LoginDialog loginDialog;
+    int result = loginDialog.exec();//模态运行
+    //检查登录结果
+    if (result == QDialog::Accepted) {
+        // 3. 登录成功：创建并显示主窗口
+        ChatMainWindow mainWindow;
+        mainWindow.show();
+
+        // 4. 进入主事件循环
+        return app.exec();
+    } else {
+        // 用户点击了“取消”或关闭窗口
+        return 0; // 正常退出
+    }
+/*    ChatMainWindow w;
     w.show();
     int result = app.exec();
     Logger::info("Application exited");
     return result;
+*/
 }
